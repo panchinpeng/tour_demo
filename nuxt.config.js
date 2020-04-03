62970a1913348ebdf7abf982f17e70e02f52f144
志鵬潘 <a84155844@gmail.com>
2020-04-04 02:13:14 +0800

    1. 修改html header meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,14 +25,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "景點分享平台",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content: "景點分享平台練習"
       }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
